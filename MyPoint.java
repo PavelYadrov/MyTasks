327f73c881b9c212ab0f87ecd3629287f786b4f7
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-05 15:59:55 +0300

    Add files via upload
    Solutions for the 1st task list

---
+++ MyPoint.java
@@ -0,0 +1,52 @@
+package com.tasks.pavelyadrov;
+
+public class MyPoint {
+    private int x=0;
+    private int y=0;
+
+    public MyPoint() {
+    }
+
+    public MyPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    public int[] getXY(){
+        return new int[] {x,y};
+    }
+    public void setXY(int x, int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    @Override
+    public String toString() {
+        return "MyPoint{" + "x=" + x + ", y=" + y + '}';
+    }
+    public double distance(int x,int y){
+        return Math.sqrt(Math.pow(this.x-x,2)+Math.pow(this.y-y,2));
+    }
+    public double distance(MyPoint another){
+        return Math.sqrt(Math.pow(this.x-another.getX(),2)+Math.pow(this.y-another.getY(),2));
+    }
+    public double distance(){
+        return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
+    }
+
+}
