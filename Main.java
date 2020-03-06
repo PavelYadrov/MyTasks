383215c77ece883240187b676e20b91b2c552405
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-07 00:19:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        //control check for the Circle class
+        //control tests for the Circle class
             System.out.println("Circle");
             Circle circle1 = new Circle();
             Circle circle2 = new Circle(3);
@@ -14,21 +13,21 @@ public class Main {
             System.out.println("Area = "+circle2.getArea()+ "  "+circle2.toString());
             System.out.println("Area = "+circle3.getArea()+ "  "+circle3.toString());
 
-        //control check for the Rectangle class
+        //control tests for the Rectangle class
             System.out.println("Rectangle");
             Rectangle rectangle1 = new Rectangle();
             Rectangle rectangle2 = new Rectangle(3,4);
             System.out.println("Area= "+ rectangle1.getArea()+" Perimeter = "+rectangle1.getPerimeter()+" "+rectangle1.toString());
             System.out.println("Area= "+ rectangle2.getArea()+" Perimeter = "+rectangle2.getPerimeter()+" "+rectangle2.toString());
 
-        //control check for the Employee class
+        //control tests for the Employee class
             System.out.println("Employee");
             Employee employee = new Employee(3,"Alexander","White",100000);
             employee.setSalary(140000);
             System.out.println(employee.getName()+" salary ="+employee.getSalary()+" AnnualSalary= "+employee.getAnnualSalary()+" RaisedSalary= " +employee.raiseSalary(15));
             System.out.println(employee);
 
-        //control check for the Book class
+        //control tests for the Book class
             Book book1 = new Book("Odyssey",new Author[]{new Author("Homer"),new Author("Homer's Brother (for example)")},1313,32);
             System.out.println(book1);
             System.out.println(book1.getAuthorsName());
@@ -36,7 +35,7 @@ public class Main {
             System.out.println(book1.getAuthors()[0]);
             System.out.println(book1.getAuthors()[1]);
 
-        //control check for the MyPoint class
+        //control tests for the MyPoint class
             MyPoint myPoint1 = new MyPoint(3,4);
             MyPoint myPoint2 = new MyPoint(2,2);
             System.out.println(Arrays.toString(myPoint1.getXY()));
@@ -45,11 +44,10 @@ public class Main {
             System.out.println(myPoint1.distance());
             myPoint1.setXY(2,2);
             System.out.println(myPoint1.distance(myPoint2));
-        */
-        //control check for the MyTriangle class
-        MyTriangle myTriangle1 = new MyTriangle(-1,0,1,0,0,2);
-        System.out.println(myTriangle1);
-        System.out.println(myTriangle1.getPerimeter());
-        System.out.println(myTriangle1.getType());
+        //control tests for the MyTriangle class
+            MyTriangle myTriangle1 = new MyTriangle(-1,0,1,0,0,2);
+            System.out.println(myTriangle1);
+            System.out.println(myTriangle1.getPerimeter());
+            System.out.println(myTriangle1.getType());
     }
 }
