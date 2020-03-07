41fa11e5248c9a0c26938127c161d418c4e1926e
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-07 23:05:11 +0300

    Add files via upload

---
+++ Ball.java
@@ -0,0 +1,72 @@
+package com.tasks.pavelyadrov;
+
+public class Ball {
+    private float x;
+    private float y;
+    private int radius;
+    private float xDelta;
+    private float yDelta;
+
+    public Ball(float x, float y, int radius,int speed,int direction) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        xDelta=(float) (speed*Math.cos(direction*Math.PI/180));
+        yDelta=(float) (-speed*Math.sin(direction*Math.PI/180));
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public float getXDelta() {
+        return xDelta;
+    }
+
+    public float getYDelta() {
+        return yDelta;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public void setXDelta(float xDelta) {
+        this.xDelta = xDelta;
+    }
+
+    public void setYDelta(float yDelta) {
+        this.yDelta = yDelta;
+    }
+    public void move(){
+        x+=xDelta;
+        y+=yDelta;
+    }
+    public void reflectHorizontal(){
+        xDelta=-xDelta;
+    }
+    public void reflectVertical(){
+        yDelta=-yDelta;
+    }
+
+    @Override
+    public String toString() {
+        return "\"Ball[("+x+","+y+"),speed=("+xDelta+","+yDelta+")]\"";
+    }
+}
