6970d0a51e372cb5c63d3d4fe7a830e474e96db5
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-13 13:16:25 +0300

    Update Author.java

--- NCtasks/Author.java
+++ NCtasks/Author.java
@@ -30,4 +30,23 @@ public class Author {
                 ", gender=" + gender +
                 '}';
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        if (gender != author.gender) return false;
+        if (name != null ? !name.equals(author.name) : author.name != null) return false;
+        return email != null ? email.equals(author.email) : author.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (int) gender;
+        return result;
+    }
 }
