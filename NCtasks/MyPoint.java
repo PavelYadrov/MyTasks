7b81f825acdee0ab090a714576c53c23571865a8
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-13 14:08:06 +0300

    Update MyPoint.java

--- NCtasks/MyPoint.java
+++ NCtasks/MyPoint.java
@@ -48,5 +48,22 @@ public class MyPoint {
     public double distance(){
         return Math.sqrt(Math.pow(this.x,2)+Math.pow(this.y,2));
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MyPoint myPoint = (MyPoint) o;
+
+        if (x != myPoint.x) return false;
+        return y == myPoint.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 
 }
