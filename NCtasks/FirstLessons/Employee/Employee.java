71ac2582b81f5a316f809364e98354b965b44823
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-03-18 21:12:14 +0300

    Add files via upload

---
+++ NCtasks/FirstLessons/Employee/Employee.java
@@ -0,0 +1,74 @@
+package com.task3.pavelyadrov;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getName(){
+        return this.firstName+" "+this.lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAnnualSalary(){
+        return this.salary*12;
+    }
+    public int raiseSalary(int percent){
+        double fullpercent = 1+ percent*0.01;
+        return (int) (salary*fullpercent);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee[" + "id=" + id + ", name='" + firstName  + " " + lastName  + ", salary=" + salary + ']';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (id != employee.id) return false;
+        if (salary != employee.salary) return false;
+        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
+        return lastName != null ? lastName.equals(employee.lastName) : employee.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + salary;
+        return result;
+    }
+}
