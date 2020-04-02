9983333cbbfe0bdf8cc25c900854e6893f68deaf
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-04-02 13:27:08 +0300

    Add files via upload

---
+++ NCtasks/Horstmann'sExercises/FourthChapter/ex7/Colors.java
@@ -0,0 +1,21 @@
+package com.exersices.ex7;
+
+public enum Colors {
+    BLACK,
+    RED,
+    BLUE,
+    GREEN,
+    CYAN,
+    MAGENTA,
+    YELLOW,
+    WHITE;
+    public static Colors getRed(){
+        return Colors.RED;
+    }
+    public static Colors getGreen(){
+        return Colors.GREEN;
+    }
+    public static Colors getBlue(){
+        return Colors.BLUE;
+    }
+}
