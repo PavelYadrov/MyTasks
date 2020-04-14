897a58ea3316deae4cee0247c3839ac700cb1e90
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-04-14 18:09:52 +0300

    Update IProductivity.java

--- NCtasks/MyCollection/ColletionsTests/IProductivity.java
+++ NCtasks/MyCollection/ColletionsTests/IProductivity.java
@@ -1,20 +1,17 @@
 package com.pavelyadrov.productivity;
 
+import com.pavelyadrov.mycollection.CollectKeys;
+import com.pavelyadrov.mycollection.ILinkedList;
 import com.pavelyadrov.mycollection.MyLinkedList;
 import com.pavelyadrov.testclasses.MyComplex;
 
 import java.util.*;
 
 public interface IProductivity extends Comparable {
-    static <T> void add(List<T> list, int n) {
-        for (int i = 0; i < n; i++) {
-            list.add((T) new Object());
-        }
-    }
 
-    static <T> void add(Set<T> list, int n) {
+    static <T> void add(Collection<T> collection,int n){
         for (int i = 0; i < n; i++) {
-            list.add((T) new Object());
+            collection.add((T) new Object());
         }
     }
 
@@ -24,97 +21,110 @@ public interface IProductivity extends Comparable {
         }
     }
 
-    static void addTree(TreeSet<String> list, int n) {
-        for (int i = 0; i < n; i++) {
-            list.add(new String("ya tyta"));
+    static <T> double[] collections(Collection<T> collection, int n, CollectKeys key) {
+        if (key== CollectKeys.SET) {
+            add(collection, n);
+            double[] res = new double[2];
+            Object c = new Object();
+            //ADD
+            double myTime = System.nanoTime();
+            collection.add((T) c);
+            myTime = myTime - System.nanoTime();
+            res[0] = -myTime;
+
+            //REMOVE
+            double myTime1 = System.nanoTime();
+            collection.remove(c);
+            myTime1 = myTime1 - System.nanoTime();
+            res[1] = -myTime1;
+            return res;
         }
-    }
 
-    static void addMap(Map<Integer, String> list, int n) {
-        for (int i = 0; i < n; i++) {
-            list.put(Integer.valueOf(i), new String("yay"));
-        }
-    }
-    static double[] arrayListAndLinked(int n) {
-        double[] res = new double[4];
-        ArrayList<Object> arrayList = new ArrayList<>();
-        add(arrayList, n);
-        LinkedList<Object> linkedList = new LinkedList<>();
-        add(linkedList, n);
-        //ADD
-        double myTime = System.nanoTime();
-        arrayList.add(n / 2, new MyComplex(11, 1));
-        myTime = System.nanoTime()-myTime;
-        double linTime = System.nanoTime();
-        linkedList.add(n / 2, new MyComplex(11, 1));
-        linTime = System.nanoTime()-linTime;
-        res[0] = myTime - linTime;
+        double[] res1 = new double[4]; //for standard collections
+        double[] res2 = new double[4]; //for MyLinkedList
 
-        //GET
-        double myTime1 = System.nanoTime();
-        arrayList.get(n / 2);
-        myTime1 = System.nanoTime()-myTime1;
-        double linTime1 = System.nanoTime();
-        linkedList.get(n / 2);
-        linTime1= System.nanoTime()-linTime1;
-        res[1] = myTime1 - linTime1;
+            ArrayList<MyComplex> arrayList = new ArrayList<>();
+            add(arrayList, n);
 
-        //SET
-        double myTime2 = System.nanoTime();
-        arrayList.set(n / 2, new MyComplex(1, 1));
-        myTime2 = System.nanoTime()-myTime2;
-        double linTime2 = System.nanoTime();
-        linkedList.set(n / 2, new MyComplex(1, 1));
-        linTime2 = System.nanoTime()-linTime2;
-        res[2] = myTime2 - linTime2;
+            LinkedList<MyComplex> linkedList = new LinkedList<>();
+            add(linkedList, n);
 
-        //REMOVE
-        double myTime3 = System.nanoTime();
-        arrayList.remove(n / 2);
-        myTime3 = System.nanoTime()-myTime3;
-        double linTime3 = System.nanoTime();
-        linkedList.remove(n / 2);
-        linTime3 = System.nanoTime()-linTime3;
-        res[3] = myTime3 - linTime3;
-        return res;
-    }
+            MyLinkedList<MyComplex> myLinkedList = new MyLinkedList<>();
+            myLinkedList.addMany(n);
 
-    static <T> double[] sets(Set<T> set, int n) {
-        add(set, n);
-        double[] res = new double[2];
-        Set<T> ns = set;
-        Object c = new Object();
-        //ADD
-        double myTime = System.nanoTime();
-        ns.add((T) c);
-        myTime = myTime - System.nanoTime();
-        res[0] = -myTime;
 
-        //REMOVE
-        double myTime1 = System.nanoTime();
-        ns.remove(c);
-        myTime1 = myTime1 - System.nanoTime();
-        res[1] = -myTime1;
-        return res;
-    }
+            //ADD
+            double myTime = System.nanoTime();
+            arrayList.add(n / 2, new MyComplex(11, 1));
+            myTime = System.nanoTime()-myTime;
+            double linTime = System.nanoTime();
+            linkedList.add(n / 2, new MyComplex(11, 1));
+            linTime = System.nanoTime()-linTime;
+            res1[0] = myTime - linTime;
 
-    static double[] sets(TreeSet<String> set, int n) {
-        addTree(set, n);
-        double[] res = new double[2];
-        TreeSet<String> ns = set;
-        String c = new String("SOS");
-        //ADD
-        double myTime = System.nanoTime();
-        ns.add(c);
-        myTime = myTime - System.nanoTime();
-        res[0] = -myTime;
+            myTime = System.nanoTime();
+            myLinkedList.add(n/2,new MyComplex(11,1));
+            myTime = System.nanoTime()-myTime;
+            linTime = System.nanoTime();
+            linkedList.add(n/2,new MyComplex(11,1));
+            linTime=System.nanoTime()-linTime;
+            res2[0]=myTime - linTime;
 
-        //REMOVE
-        double myTime1 = System.nanoTime();
-        ns.remove(c);
-        myTime1 = myTime1 - System.nanoTime();
-        res[1] = -myTime1;
-        return res;
+            //GET
+            double myTime1 = System.nanoTime();
+            arrayList.get(n / 2);
+            myTime1 = System.nanoTime()-myTime1;
+            double linTime1 = System.nanoTime();
+            linkedList.get(n / 2);
+            linTime1= System.nanoTime()-linTime1;
+            res1[1] = myTime1 - linTime1;
+
+            myTime = System.nanoTime();
+            myLinkedList.get(n/2);
+            myTime = System.nanoTime()-myTime;
+            linTime = System.nanoTime();
+            linkedList.get(n/2);
+            linTime=System.nanoTime()-linTime;
+            res2[1]= myTime-linTime;
+
+            //SET
+            double myTime2 = System.nanoTime();
+            arrayList.set(n / 2, new MyComplex(1, 1));
+            myTime2 = System.nanoTime()-myTime2;
+            double linTime2 = System.nanoTime();
+            linkedList.set(n / 2, new MyComplex(1, 1));
+            linTime2 = System.nanoTime()-linTime2;
+            res1[2] = myTime2 - linTime2;
+
+            myTime = System.nanoTime();
+            myLinkedList.set(n/2,new MyComplex(1,1));
+            myTime = System.nanoTime()-myTime;
+            linTime = System.nanoTime();
+            linkedList.set(n/2,new MyComplex(1,1));
+            linTime=System.nanoTime()-linTime;
+            res2[2]= myTime-linTime;
+
+            //REMOVE
+            double myTime3 = System.nanoTime();
+            arrayList.remove(n / 2);
+            myTime3 = System.nanoTime()-myTime3;
+            double linTime3 = System.nanoTime();
+            linkedList.remove(n / 2);
+            linTime3 = System.nanoTime()-linTime3;
+            res1[3] = myTime3 - linTime3;
+
+            myTime = System.nanoTime();
+            myLinkedList.remove(n/2);
+            myTime =System.nanoTime()-myTime;
+            linTime = System.nanoTime();
+            linkedList.remove(n/2);
+            linTime=System.nanoTime()-linTime;
+            res2[3]= myTime-linTime;
+
+            if (key==CollectKeys.MYLINKEDLIST){
+                return res2;
+            }
+            else return res1;
     }
 
     static <K, V> double[] maps(Map<K, V> map, int n) {
@@ -150,38 +160,4 @@ public interface IProductivity extends Comparable {
         return res;
     }
 
-    static <K, V> double[] maps(TreeMap<Integer, String> map, int n) {
-        addMap(map, n);
-        double[] res = new double[4];
-        Map<Integer, String> ns = map;
-        Integer c1 = new Integer(3);
-        String c2 = new String("yays");
-
-        //ADD
-        double myTime = System.nanoTime();
-        ns.put(c1, c2);
-        myTime = myTime - System.nanoTime();
-        res[0] = -myTime;
-
-        //GET
-        double myTime1 = System.nanoTime();
-        ns.get(c1);
-        myTime1 = myTime1 - System.nanoTime();
-        res[1] = -myTime1;
-
-        //REPLACE
-        double myTime2 = System.nanoTime();
-        ns.replace(c1, c2, "new Object()");
-        myTime2 = myTime2 - System.nanoTime();
-        res[2] = -myTime2;
-
-        //REMOVE
-        double myTime3 = System.nanoTime();
-        ns.remove(c1, c2);
-        myTime3 = myTime3 - System.nanoTime();
-        res[3] = -myTime3;
-        return res;
-
-
-    }
 }
