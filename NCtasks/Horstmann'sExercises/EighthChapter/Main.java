23c6dce507e58307a5200495fc023ad70a12fb24
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-04-14 11:28:52 +0300

    Update Main.java

--- NCtasks/Horstmann'sExercises/EighthChapter/Main.java
+++ NCtasks/Horstmann'sExercises/EighthChapter/Main.java
@@ -2,37 +2,42 @@ package com.exercises;
 
 import com.exercises.ex7.Tokens;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        //TEST EVERYTHING APART
-        //THERE IS ONLY 1 STREAM FOR ALL CLASSES AND IT MAY CAUSE IllegalStateException FOR GROUP TEST
 
         Tokens tokens = new Tokens("B:\\myNCprojects\\HorstmanExercises\\Tolstoy Leo. War and Peace.txt");
-        
+
         //Seventh exercise
+        System.out.println("\nSeventh Exercise");
         System.out.println(tokens.getHundred());
-        
+
         //Eighth exercise
+        System.out.println("\nEighth Exercise");
         List<Object> bh = new ArrayList<>();
         IntStream.range(100,10000).mapToObj(Optional::of).map(op -> op.filter(x -> x % 2 == 0))
                 .flatMap(Stream::of).forEach(s->s.ifPresent(bh::add));
         System.out.println(bh);
 
         //Ninth exercise
+        System.out.println("\nNinth Exercise");
         System.out.println(tokens.getFive());
 
         //Tenth exercise
+        System.out.println("\nTenth Exercise");
         System.out.println(tokens.average());
 
         //Eleventh exercise
+        System.out.println("\nEleventh Exercise");
         System.out.println(tokens.maxLength());
 
 
        //Fifteenth exercise
+        System.out.println("\nFifteenth Exercise");
         List<Double> doubleList= new ArrayList<>();
         doubleList.add(3.2);
         doubleList.add(2.3);
@@ -46,12 +51,15 @@ public class Main {
         doubleList.add(5.0);
         double average = doubleList.stream().reduce((s1,s2)-> (s1+s2)/2).orElse(0.0);
         System.out.println(average);
-        
+
         //Sixteenth exercise
+        System.out.println("\nSixteenth Exercise");
         System.out.println(Tokens.primes());
-       
+
         //Seventeenth exercise
+        System.out.println("\nSeventeenth Exercise");
         System.out.println(tokens.fiveHundredStrings());
+
 
 
 
