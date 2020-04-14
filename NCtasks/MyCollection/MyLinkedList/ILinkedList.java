b21f7115bd3f2ecb0b2b055d343338a6efe28bb1
PavelYadrov <61824537+PavelYadrov@users.noreply.github.com>
2020-04-14 18:11:26 +0300

    Update ILinkedList.java

--- NCtasks/MyCollection/MyLinkedList/ILinkedList.java
+++ NCtasks/MyCollection/MyLinkedList/ILinkedList.java
@@ -1,5 +1,6 @@
 package com.pavelyadrov.mycollection;
 
+import com.pavelyadrov.productivity.IProductivity;
 import com.pavelyadrov.testclasses.MyComplex;
 
 import java.util.*;
@@ -44,57 +45,10 @@ public interface ILinkedList<T> extends Iterable<T> {
                this.add((T) new MyComplex(k,l));
           }
      }
-     default void addMany(List<T> list, int n){
-          for (int i = 0; i <n; i++) {
-               int k = rand.nextInt(1000);
-               int l = rand.nextInt(1000);
-               list.add((T) new MyComplex(k,l));
-          }
+
+     //Compare method
+      static void productivityCompare(int n){
+          IProductivity.collections(new ArrayList<>(),n,CollectKeys.MYLINKEDLIST);
      }
-     //only for List with MyComplex Objects
-     //cuz i didn't have much time, sorry
-      static double productivityCompare(int n){
-          MyLinkedList<MyComplex> myLinkedList = new MyLinkedList<>();
-          myLinkedList.addMany(n);
-          LinkedList<MyComplex> linkedList = new LinkedList<MyComplex>();
-          myLinkedList.addMany(linkedList,n);
-
-          //add time Difference with "+n+" elements like MyLinkedList(Time)-LinkedList(Time)
-
-           double myTime = System.nanoTime();
-           myLinkedList.add(n/2,new MyComplex(11,1));
-           myTime = System.nanoTime()-myTime;
-           double linTime = System.nanoTime();
-           linkedList.add(n/2,new MyComplex(11,1));
-           linTime=System.nanoTime()-linTime;
-           return myTime - linTime;
-
-           //get time Difference with "+n+" elements like MyLinkedList(Time)-LinkedList(Time)
-           /*double myTime = System.nanoTime();
-           myLinkedList.get(n/2);
-           myTime = System.nanoTime()-myTime;
-           double linTime = System.nanoTime();
-           linkedList.get(n/2);
-           linTime=System.nanoTime()-linTime;
-           return myTime-linTime;*/
-
-           //set time Difference with "+n+" elements like MyLinkedList(Time)-LinkedList(Time)
-           /*double myTime = System.nanoTime();
-           myLinkedList.set(n/2,new MyComplex(1,1));
-           myTime = System.nanoTime()-myTime;
-           double linTime = System.nanoTime();
-           linkedList.set(n/2,new MyComplex(1,1));
-          linTime=System.nanoTime()-linTime;
-           return myTime-linTime;*/
-
-          //remove time Difference with "+n+" elements like MyLinkedList(Time)-LinkedList(Time)
-        /*  double myTime = System.nanoTime();
-          myLinkedList.remove(n/2);
-          myTime =System.nanoTime()-myTime;
-          double linTime = System.nanoTime();
-          linkedList.remove(n/2);
-          linTime=System.nanoTime()-linTime;
-          return myTime-linTime;*/
-     }
-
 }
+
